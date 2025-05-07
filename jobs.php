ce81439aa47e9b60a7a0d4ecccf3f99719486368
102770761 <102770761@student.swin.edu.au>
2025-05-07 12:13:33 +1000

    Added modularity to the nav bar, header and footer

--- jobs.php
+++ jobs.php
@@ -18,23 +18,11 @@
 
 <body>
     <header>
-        <section>
-            <!-- OpenSOS Logo -->
-            <a href="index.html"> <!--Link Logo to Home Page-->
-                <img src="images/tab_icon.png" alt="OpenSOS logo" id="header_logo_not_homepage">
-            </a>
-            <!-- Heading -->
-            <h1 id="heading_not_homepage">Jobs Descriptions</h1>
-        </section>
-
-        <!-- The navigation bar -->
-        <!-- The navigation menu should be the same for all pages -->
-        <nav>
-            <a href="index.html" title="Homepage">Homepage</a>
-            <a href="jobs.html" title="Job Descriptions">Job Descriptions</a>
-            <a href="apply.html" title="Apply">Apply</a>
-            <a href="about.html" title="About Us">About Us</a>
-        </nav>
+        <?php
+            $page_title = "Job Descriptions"; // Set the page title
+            include 'header.inc'; // Include the header file
+        ?>
+        <?php include 'nav.inc' ?> <!--Include Navigation Bar-->
         <br>
     </header>
 
@@ -269,11 +257,6 @@
         </div>
     </main>
   
-     <!--Footer-->
-     <footer>
-        <p>&copy; OpenSOS 2025. All rights reserved.</p>
-        <p><a href="https://opensos.atlassian.net/jira/software/projects/SCRUM/boards/1" title="Go To Jira Board" id="jira_link" target="_blank"><em>Jira Board</em></a></p>
-        <p><a href="https://github.com/102153870/openSOS_groupProjectPart1" title="Go to Github Repository" id="github_link" target="_blank"><em>Github Link</em></a></p>
-    </footer>
+    <?php include 'footer.inc' ?> <!--Include Footer-->
 </body>
 </html>
