bbbf3948a4ed299c5637cbb479536113f6e06cbe
102770761 <102770761@student.swin.edu.au>
2025-05-10 14:36:57 +1000

    First Iteration of manager login

---
+++ manage.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+// Add authentication check here
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Manager Dashboard - OpenSOS</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <h1>Manager Dashboard</h1>
+    <p>Welcome to the manager dashboard!</p>
+</body>
+</html>
