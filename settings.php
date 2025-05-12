5c655e5a4dcb4e8e67eb379f3797ff54c28074ff
102770761 <102770761@student.swin.edu.au>
2025-05-12 15:52:26 +1000

    Tried with own database (opensos)

--- settings.php
+++ settings.php
@@ -1,8 +1,7 @@
 <?php
 // settings.php to store connection variables
-
-$host = "localhost";      // or "127.0.0.1"
-$user = "your_db_user";   // e.g., "root"
-$pwd  = "your_password";  // e.g., ""
-$sql_db = "your_database_name"; // e.g., "opensos_db"
+$host = "localhost";     
+$user = "root";   
+$password  = ""; 
+$sql_db = "opensos";  
 ?>
