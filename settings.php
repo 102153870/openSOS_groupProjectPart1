8b5be477fd74b5a9ac3a436569f3bbfd176a836f
102770761 <102770761@student.swin.edu.au>
2025-05-13 19:25:46 +1000

    added proper conn variable to settings.php

--- settings.php
+++ settings.php
@@ -3,5 +3,12 @@
 $host = "localhost";     
 $user = "root";   
 $password  = ""; 
-$sql_db = "opensos";  
+$sql_db = "OPENSOS"; // Database name
+
+$conn = mysqli_connect($host, $user, $password, $sql_db);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
+
