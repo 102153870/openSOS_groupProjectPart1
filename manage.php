cb458ae34b6737890cc1956381d3a81f9227eb2f
102770761 <102770761@student.swin.edu.au>
2025-05-14 18:42:34 +1000

    Added redirect to index.php if user is not logged in

--- manage.php
+++ manage.php
@@ -9,6 +9,20 @@ if (!isset($_SESSION['username'])) {
 
 // Get username from session
 $username = $_SESSION['username'];
+
+//Check if logout button is pressed
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
+    session_unset();         // Unset all session variables
+    session_destroy();       // Destroy the session
+    header('Location: index.php'); // Redirect to login or another page
+    exit;
+}
+
+//SECURITY: Check if user is a manager, if not redirect to index.php
+if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] != 'manager') {
+    header("Location: index.php");
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
@@ -19,8 +33,13 @@ $username = $_SESSION['username'];
     <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
-    <h1>Manager Dashboard</h1>
-        <p>Welcome to the manager dashboard  <?php echo htmlspecialchars($username) ?>!</p>;
+    <?php 
+    $page_title = "Dashboard";
+    include 'header.inc'; ?>
 
+        <p>Welcome to the manager dashboard  <?php echo htmlspecialchars($username) ?>!</p>
+        <form method="post">
+            <button type="submit" name="logout" class="buttons">Logout</button> <!-- Logout button -->
+        </form>
 </body>
 </html>
