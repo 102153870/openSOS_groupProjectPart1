8b0bd4aa0266282c9faf9292c3bed5038b0c1730
ryangetgit <105914892@student.swin.edu.au>
2025-05-21 18:39:28 +1000

    added screenshot for manager registration page

--- enhancements.php
+++ enhancements.php
@@ -45,7 +45,7 @@ include 'nav.inc'; // Include the navigation bar
                 <a href="register_manager.php" class="buttons_description_box">View Enhancement</a>
             </section>
             <section class="job_description_right">
-                <img src="" alt="Manager Registration Page">
+                <img src="images/manager_registration.png" alt="Manager Registration Page">
             </section>
         </div>
 
